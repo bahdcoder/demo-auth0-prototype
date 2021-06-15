b8020edc4afc962644bee79edd9fef322a28a2af
Kati Frantz <bahdcoder@gmail.com>
2021-06-15 18:40:45 +0100

    remove config import

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./views/Home";
 import Profile from "./views/Profile";
-import ExternalApi from "./views/ExternalApi";
 import { useAuth0 } from "@auth0/auth0-react";
 import history from "./utils/history";
 
